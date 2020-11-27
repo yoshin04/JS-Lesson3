208c85dc508ae5b13c3e8c4dd9d0a8cc42eaf509
yoshin04 <taigaby0411@gmail.com>
2020-11-27 23:29:02 +0900

    foreach ver

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
 'use strict';
-  const taskTrigger = document.getElementById('task_trigger');
-  const taskTarget = document.getElementById('task_target');
-  const taskValue = document.getElementById('task_value');
-  let  taskId = 0;
+const taskTrigger = document.getElementById('task_trigger');
+const taskTarget = document.getElementById('task_target');
+const taskValue = document.getElementById('task_value');
+const todos = [];
 
-  const addTask = (task, id) => {
-      const tableItem = document.createElement('tr');
-      taskTarget.appendChild(tableItem);
-      const taskId = document.createElement('td');
-      taskId.innerText = id;
-      tableItem.appendChild(taskId);
-      const taskContent = document.createElement('td');
-      taskContent.innerText = task;
-      tableItem.appendChild(taskContent);
-      const status = document.createElement('td');
-      tableItem.appendChild(status);
-      const statusButton = document.createElement('button');
-      statusButton.innerText = '作業中';
-      status.appendChild(statusButton);
-      const remove = document.createElement('td');
-      tableItem.appendChild(remove);
-      const removeButton = document.createElement('button');
-      removeButton.innerText = '削除';
-      remove.appendChild(removeButton);
-  } 
-    
-
-  taskTrigger.addEventListener('click', () => {
-    const task = taskValue.value;
-    addTask(task, taskId++);
-    taskValue.value = '';
+const addTask = () => {
+  todos.forEach((todo, index) => {
+    const tableItem = document.createElement('tr');
+    taskTarget.appendChild(tableItem);
+    const taskId = document.createElement('td');
+    taskId.innerText = index;
+    tableItem.appendChild(taskId);
+    const taskContent = document.createElement('td');
+    taskContent.innerText = todo.task;
+    tableItem.appendChild(taskContent);
+    const status = document.createElement('td');
+    tableItem.appendChild(status);
+    const statusButton = document.createElement('button');
+    statusButton.innerText = todo.status;
+    status.appendChild(statusButton);
+    const remove = document.createElement('td');
+    tableItem.appendChild(remove);
+    const removeButton = document.createElement('button');
+    removeButton.innerText = '削除';
+    remove.appendChild(removeButton);
   });
-  
+};
+
+taskTrigger.addEventListener('click', () => {
+  const comment = taskValue.value;
+  const button = '作業中';
+  const todo = { task:comment, status:button}
+  todos.push(todo);
+  addTask(todos);
+  taskValue.value = '';
+});
