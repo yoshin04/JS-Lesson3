7347d2adced98efe3ce033b17b65cd0798b341b6
yoshin04 <taigaby0411@gmail.com>
2020-12-10 11:41:00 +0900

    3-4使用通りの動作に修正

--- script.js
+++ script.js
@@ -10,12 +10,16 @@ const createStatusButton = (todo, status, row) => {
   status.appendChild(statusButton);
   statusButton.addEventListener('click', () => {
     if (statusButton.innerText === '作業中') {
-      statusButton.innerText = '完了';
+      todo.status = '完了';
+      statusButton.innerText = todo.status;
       row.classList.add('finish');
+      change();
       return;
     }
-    statusButton.innerText = '作業中';
+    todo.status = '作業中';
+    statusButton.innerText = todo.status;
     row.classList.remove('finish');
+    change();
   });
 }
 
@@ -27,9 +31,7 @@ const createRemoveButton = (remove, row) => {
     const index = row.rowIndex - 1;
     todos.splice(index, 1);
     while (table.rows[0]) table.deleteRow(0);
-    todos.forEach((todo) => {
-      showTasks(todo);
-    });
+    showTasks();
   });
 }
 
@@ -62,30 +64,32 @@ const change = () => {
   }
 }
 
-const showTasks = todo => {
-  const taskId = taskTable.rows.length;
-
-  //最後の行に新しい行を追加
-  const row = taskTable.insertRow(-1);
-  row.classList.add('tasks');
-
-  //各項目の追加
-  const id = row.insertCell(0);
-  const comment = row.insertCell(1);
-  const status = row.insertCell(2);
-  const remove = row.insertCell(3);
-
-  id.innerText = taskId;
-  comment.innerText = todo.task;
-  createStatusButton(todo, status, row);
-  createRemoveButton(remove, row);
+const showTasks = () => {
+  taskTable.innerText = '';
+  todos.forEach((todo) => {
+    const taskId = taskTable.rows.length;
+    //最後の行に新しい行を追加
+    const row = taskTable.insertRow(-1);
+    row.classList.add('tasks');
+  
+    //各項目の追加
+    const id = row.insertCell(0);
+    const comment = row.insertCell(1);
+    const status = row.insertCell(2);
+    const remove = row.insertCell(3);
+  
+    id.innerText = taskId;
+    comment.innerText = todo.task;
+    createStatusButton(todo, status, row);
+    createRemoveButton(remove, row);
+  });
 }
 
 //task追加機能発火
 taskTrigger.addEventListener('click', () => {
   const todo = { task: taskValue.value, status: '作業中' }
   todos.push(todo);
-  showTasks(todo);
+  showTasks();
   taskValue.value = '';
 });
 
